ed612e1702f20ee3ddbb83f4716a5dfd787c8287
sinan <aslansinan586@gmail.com>
2022-12-31 19:55:02 +0300

    using useCallback and passing data with prevstate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import './App.css';
-import { useState,useMemo } from 'react';
-import Header from './components/Header';
+import "./App.css";
+import { useState, useMemo, useCallback } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [number,SetNumber] = useState(0);
-  const [text,setText] =  useState("");
-  const data = useMemo(() => {
-    return calculateObject(number);
-  }, [number])
+  const [number, SetNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  const increment = useCallback(() => {
+       SetNumber((prevState) => prevState + 1); // buradaki yapı ile tekrar tekrar çağorma işlemi olmaz.
+  },[])
+
+  // const data = useMemo(() => {
+  //   return calculateObject(number);
+  // }, [number])
   return (
     <div className="App">
-    <Header data={data}/><hr />
-    <h1>{number}</h1>
-    <button onClick={() => SetNumber(number + 1)}>Click</button>
-  <br /> <br />
-  <input value={text} onChange= {({target}) => setText(target.value)} />
+      <Header increment={increment} />
+      <hr />
+      <h1>{number}</h1>
+      <input value={text} onChange={({ target }) => setText(target.value)} />
+      <br /> <br />
+     
     </div>
   );
 }
-function calculateObject(number){
-  console.log("Calculating");
-  for(let i =0;i<10000000; i++){}
-  console.log("Calculating complated");
-  return {name:'Mehmet',number}
-}
+// function calculateObject(number) {
+//   console.log("Calculating");
+//   for (let i = 0; i < 10000000; i++) {}
+//   console.log("Calculating complated");
+//   return { name: "Mehmet", number };
+// }
 export default App;
